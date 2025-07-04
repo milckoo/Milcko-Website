2968218f34a0f23398109317f4340be9e9d7a984
Ayush Bunkar <ayushbunkar100@gmail.com>
2025-07-05 01:14:00 +0530

    second page done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import FarmersSection from '../components/FarmersSection'; 
+import FarmersSection from '../components/FarmersSection'; // ✅ Import it
 
 const Home = () => {
-  return <Hero />;
-   <FarmersSection /> 
+  return (
+    <div>
+      <Hero />
+      <FarmersSection /> {/* ✅ Call the component */}
+    </div>
+  );
 };
 
 export default Home;
